42ae68bb85a5fad2e8a13f96d9107d8b07769720
arrio-o <arrio@ya.ru>
2016-05-19 17:54:12 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace RPGbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var RPGbot = new DiscordClient();
+
+            RPGbot.MessageReceived += (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
+                    Console.WriteLine(e.User.Name + " said " + e.Message.Text);
+            };
+
+            RPGbot.ExecuteAndWait(async () =>
+            {
+                await RPGbot.Connect("");
+            });
+        }
+    }
+}
